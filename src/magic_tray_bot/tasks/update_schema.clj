42ff8571038d7d5bfba016b0eeef2a9c1b012f64
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2023-09-21 18:44:26 +0530

    Schema reading from .edn

--- src/magic_tray_bot/tasks/update_schema.clj
+++ src/magic_tray_bot/tasks/update_schema.clj
@@ -1,9 +1,10 @@
 (ns magic-tray-bot.tasks.update-schema
-  (:require [clojure.tools.namespace.find :refer [find-namespaces-in-dir]]
-            [datomic.api :as d]
-            [dialog.logger :as log]
-            [magic-tray-bot.db :as db])
-  (:gen-class))
+    (:require [datomic.api :as d]
+              [dialog.logger :as log]
+              [magic-tray-bot.db :as db]
+              [clojure.java.io :as io]
+              [clojure.edn :as edn])
+    (:gen-class))
 
 (defn- req&->name!
   [ns]
@@ -11,14 +12,19 @@
   (require ns)
   (name ns))
 
+(defn debug-
+  [expr]
+  (log/debug expr)
+  expr)
+
 (defn update-schema
   "Updates schema with NEW data. Not removing unactual." ; TODO: Develop full update
   []
   (log/info "Updating schema...")
-  (let [schema (->> (find-namespaces-in-dir (clojure.java.io/file "./src/magic_tray_bot/schema"))
-                    (map req&->name!)
-                    (filter #(some? (resolve (symbol % "schema"))))
-                    (mapcat #(load-string (str % "/schema")))
+  (let [transf #(->> % (io/reader) (java.io.PushbackReader.) (edn/read))
+        schema (->> (file-seq (io/file "./src/magic_tray_bot/schema"))
+                    (filter #(= (re-find #"\.[a-zA-Z0-9]+$" (.getName %)) ".edn"))
+                    (mapcat transf)
                     (vec))]
     (log/debug "Schema:" schema)
     (let [f (d/transact (db/conn) schema)]
