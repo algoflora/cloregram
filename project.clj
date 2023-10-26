a5668368446471ed01d841a251dac53af0a2b952
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2023-10-26 18:57:22 +0530

    https in link

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cloregram "0.2.4"
   :description "Clojure/Datomic framework for making complex Telegram Bots/Applications"
-  :url "http://cloregram.io"
+  :url "https://cloregram.io"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
