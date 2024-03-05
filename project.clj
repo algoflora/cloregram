e0d5b0208044c3f83a44fb0f19cb5f14c3d79601
Sasha <40373781+algoflora@users.noreply.github.com>
2024-03-05 23:10:58 +0530

    algoflora group in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloregram "0.7.1"
+(defproject algoflora/cloregram "0.7.1"
   :description "Clojure/Datomic framework for making complex Telegram Bots/Applications"
   :url "https://cloregram.io"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
